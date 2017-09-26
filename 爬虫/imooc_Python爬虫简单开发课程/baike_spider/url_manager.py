67ef33bf5e2c4924704d652159215d38461f67a7
kaiyikang <kaiyikang@gmail.com>
2017-09-26 16:36:44 +0800

    immoc爬虫
    add url Manager and Function of set()

--- 爬虫/imooc_Python爬虫简单开发课程/baike_spider/url_manager.py
+++ 爬虫/imooc_Python爬虫简单开发课程/baike_spider/url_manager.py
@@ -1,13 +1,32 @@
 class UrlManager(object):
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
 
-    def add_new_url(self, root_url):
-        pass
 
+    # 添加url
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+
+
+    # 添加很多url
+    def add_new_urls(self,urls):
+        if urls is None or len(urls) == 0 :
+            return
+        for url in urls:
+            self.add_new_url(url)
+
+    # 判断是否有待爬取的url
+    # 如果new urls 列表的长度不为0.则证明有还有要爬取的url
     def has_new_url(self):
-        pass
+        return len(self.new_urls) != 0
 
+    # 获得代爬取的url
     def get_new_url(self):
-        pass
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
 
-    def add_new_urls(self):
-        pass
