69da04ce48ce39b78cd53d3326027a16c6e01056
kaiyikang <kaiyikang@gmail.com>
2017-09-28 11:45:12 +0800

    add 注释
    对于parser 添加 urljoin的注释

--- 爬虫/imooc_Python爬虫简单开发课程/baike_spider/html_parser.py
+++ 爬虫/imooc_Python爬虫简单开发课程/baike_spider/html_parser.py
@@ -2,7 +2,7 @@ import re
 import urllib.parse
 from bs4 import BeautifulSoup
 
-class HtmlParser(object):
+class HtmlParser (object):
 
     def parse(self, page_url, html_cont):
         if page_url is None or html_cont is None:
@@ -16,28 +16,31 @@ class HtmlParser(object):
     # 获取新的urls
     def _get_new_urls (self, page_url, soup):
 
-        new_urls = set()
+        new_urls = set() # /item/123.html
         links = soup.find_all('a',href=re.compile(r'/item'))
 
         for link in links:
             new_url = link['href']
-            # url 拼接,具体的不太明白
+            # 让 new_url 以 page_url 为模板拼接成一个全新的 url
             new_full_url = urllib.parse.urljoin(page_url,new_url)
             new_urls.add(new_full_url)
+
+            # page_url  = https://baike.baidu.com/item/Python
+            # new_url = /item/HTML
+            # 在经过了urljoin的拼接之后
+            # 得到https://baike.baidu.com/item/HTML
         return new_urls
 
-    #获取新的数据
+    # 获取新的数据
     def _get_new_data (self, page_url, soup):
 
         res_data = {}
 
-        # url
+
         res_data['url'] = page_url
 
-
-
         title_node = soup.find('dd', class_='lemmaWgt-lemmaTitle-title').find('h1')
-        if title_node is None:
+        if title_node is None: #如果title node 没有，直接跳过
             res_data['title'] = ''
             res_data['summary'] = ''
             return res_data
@@ -53,5 +56,5 @@ class HtmlParser(object):
 
 
         return res_data
-
+        
         # 这个方法解析出两个数据：新的url列表 和 数据
